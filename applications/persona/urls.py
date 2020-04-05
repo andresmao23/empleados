56baef64b760c871ab4a61e750d75bf88bfa1684
Mao <amcaicedo@unimayor.edu.co>
2020-04-05 16:15:53 -0500

    Implementada la Vista Basada en Clase ListView lara listar los empleados

--- applications/persona/urls.py
+++ applications/persona/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ListAllEmpleadosView
+
+app_name = 'persona'
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='index'),
+    path('list-all-empleados/', ListAllEmpleadosView.as_view(), name='list-all')
 ]
--- applications/persona/views.py
+++ applications/persona/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from .models import Empleado
+from django.views.generic import (
+    TemplateView,
+    ListView
+    )
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class ListAllEmpleadosView(ListView):
+    template_name = "persona/list_all.html"
+    model = Empleado
+    context_object_name = 'lista'
